f82cea029a6a78c73156a021d7564c8fee539973
suda-morris <362953310@qq.com>
2018-04-22 21:08:16 +0800

    save config files for kernel and buildroot

--- kernel/linux-3.4.y/include/generated/compile.h
+++ kernel/linux-3.4.y/include/generated/compile.h
@@ -1,6 +1,6 @@
-/* This file is auto generated, version 11 */
+/* This file is auto generated, version 13 */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#11 Sun Apr 22 13:50:28 CST 2018"
+#define UTS_VERSION "#13 Sun Apr 22 21:06:13 CST 2018"
 #define LINUX_COMPILE_BY "morris"
 #define LINUX_COMPILE_HOST "morris"
 #define LINUX_COMPILER "gcc version 4.8.3 20131202 (prerelease) (Hisilicon_v300) "
